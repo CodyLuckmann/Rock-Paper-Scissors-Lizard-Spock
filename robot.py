d84579c55460864f982e6e448cb2801b3190ea62
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-11-16 14:47:39 -0600

    Update player attributes.

--- robot.py
+++ robot.py
@@ -1,8 +1,17 @@
 from utilities import Utilites
 from random import Random
+from player import Player 
 
-
-class Robot:
+class Robot(Player):
     def __init__(self, name):
         self.name = name 
         pass
+    
+    def display_and_pick_gestures():
+        random.choice(gestures_list)
+        ran1 = random.choice(gestures_list)
+        result = ran1
+        return result
+                 
+
+        pass 
