79aed52f73a3120f42e3702e04918dd66871971c
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-11-16 11:24:47 -0600

    test
    test colab commit

--- utilities.py
+++ utilities.py
@@ -2,3 +2,8 @@ class Utilites:
     
 rules = "The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock"
 print(rules) 
+
+
+
+
+#Test colab commit
