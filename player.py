55e406391e655d37cb8acd722cdca2947276eabf
CodyLuckmann <cluckmann@live.com>
2021-11-16 15:33:48 -0600

    Edited display gesture input and moved gesture list inside of Player class

--- player.py
+++ player.py
@@ -3,18 +3,15 @@ from utilities import Utilites
 from robot import Robot
 from human import Human
 
-gestures_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-
 
 class Player:
     def __init__(self, name=None):
-    # name
         self.name = name
         
-        _gestures_list =["Rock", "Paper"]
+        _gestures_list =["Rock", "Paper", "Scissors", "Lizard", "Spock"]
         
     def display_and_pick_gestures(self):
-        gesture = int(input('rock = 1 \n paper = 2 \n scissors = 3 \n lizard = 4 \n spock = 5'))
+        gesture = int(input('Please pick a gesture: \nrock = 1 \n paper = 2 \n scissors = 3 \n lizard = 4 \n spock = 5'))
         return gesture
 
     
