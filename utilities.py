e9cb8346e1f5b9aef7dd1ee7d3601e7421c1febc
CodyLuckmann <cluckmann@live.com>
2021-11-16 11:28:51 -0600

    Created method to display rules

--- utilities.py
+++ utilities.py
@@ -1,7 +1,8 @@
 class Utilites:
-    
-rules = "The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock"
-print(rules) 
+ 
+ def display_rules():   
+    rules = "The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock"
+    print(rules) 
 
 
 
