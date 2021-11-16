7f985541650bf5fd3b78949826816d67409bb6cb
CodyLuckmann <cluckmann@live.com>
2021-11-16 13:31:51 -0600

    Created welcome statement

--- utilities.py
+++ utilities.py
@@ -1,14 +1,14 @@
 class Utilites:
     
     gestures_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
- 
+    
+def display_welcome():
+    welcome = "Welcome to RPSLS!"    
+    print(welcome)
+    
 def display_rules():   
     rules = "The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock"
     print(rules) 
-
-def user_name():
-    input("Please enter your name: ")
-    return input
 
 def choose_game_mode():
     int(input("For single player press: 1 \n For multiplayer press: 2 "))
@@ -16,6 +16,10 @@ def choose_game_mode():
         return 1
     else:
         return 2
+    
+def user_name():
+    input("Please enter your name: ")
+    return input
 
     
 
