a52da47fdb080e9e33ebb6987e41b6827e9e8d80
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-11-16 14:48:45 -0600

    Update human attributes

--- human.py
+++ human.py
@@ -1,7 +1,10 @@
 from utilities import Utilites
+from player import Player 
 
-
-class Human:
+class Human(Player):
     def __init__(self, name):
         self.name = name 
         pass
+    
+    def display_and_pick_gestures(self):
+    
