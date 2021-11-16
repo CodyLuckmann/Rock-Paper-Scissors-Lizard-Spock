908451f0b6d8db3831975699bed3564bc0df928e
CodyLuckmann <cluckmann@live.com>
2021-11-16 10:55:48 -0600

    Added utilites class, and rules for the game

---
+++ utilities.py
@@ -0,0 +1,4 @@
+class Utilites:
+    
+rules = "The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock"
+print(rules) 
