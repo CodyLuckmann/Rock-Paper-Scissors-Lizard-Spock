6d59f02f96035338d6a408d06aa04b7ccd4478d7
CodyLuckmann <cluckmann@live.com>
2021-11-17 11:19:12 -0600

    Added spaces to make lines more clear

--- utilities.py
+++ utilities.py
@@ -2,26 +2,26 @@ class Utilites:
     
     gestures_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
     
-def display_welcome():
-    print("Welcome to RPSLS!")    
-    
-def display_rules():   
-    print("The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock")
+    def display_welcome():
+        print("\n\nWelcome to RPSLS!\n")    
+        
+    def display_rules():   
+        print("The rules are: \nRock beats Scissors \nScissors beats Paper \nPaper beats Rock \nRock beats Lizard \nLizard beats Spock \nSpock beats Scissors \nScissors beats Lizard \nLizard beats Paper \nPaper beats Spock \nSpock beats Rock\n")
 
 
-def choose_game_mode():
-    int(input("For single player press: 1 \n For multiplayer press: 2 "))
-    if input == 1:
-        return 1
-    else:
-        return 2
-    
-def user_name():
-    Player_one = input("Please enter your name: ")
-    return input
+    def choose_game_mode():
+        int(input("\nFor single player press: 1  \nFor multiplayer press: 2 "))
+        if input == 1:
+            return 1
+        else:
+            return 2
+        
+    def user_name():
+        Player_one = input("Please enter your name: ")
+        return input
 
-def display_game_winner():
-    print('Wins!!')
+    def display_game_winner():
+        print('Wins!!')
 
     
 
