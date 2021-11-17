21e2f132f98ba77c1c241a1b5955e6ab3cc2d0f8
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-11-17 10:29:28 -0600

    Updates to game run functions

--- main.py
+++ main.py
@@ -7,3 +7,45 @@ from random import Random
 
 # run game
 RPSLS.gun_game()
+
+
+def player_choice():
+    player1_choice = 0 
+    player2_choice = 1
+    
+    if (player1_choice == 1 and player2_choice == 3 4):  
+        playr1_choice == True  
+        print("Rock beats scissors")
+    if (player1_choice == 1 and player2_choice == 4):   
+        playr1_choice == True  
+        print("Rock beats lizard")
+    elif (player1_choice == 2 and player2_choice == 1 or 5):  
+        playr1_choice == True 
+    elif (player1_choice == 3 and player2_choice == 2 or 4): 
+        playr1_choice == True 
+    elif (player1_choice == 4 and player2_choice == 5 or 2): 
+        playr1_choice == True 
+    elif (player1_choice == 5 and player2_choice == 1 or 3): 
+        player1_choice == True   
+        print("Rock beats scissors/lizard")
+    else:
+        print("Players have tied. ")
+        player1_choice()    
+    result = player1_choice
+    return result 
+
+    return "player1 wins" 
+    return "player2 wins" 
+    
+
+        playr1_choice == True  
+        print("Rock beats scissors/lizard")
+        
+
+if player1_choice == 1:
+    if player2_choice == 1
+        print("Its a tie")
+    elif player2_choice == 2
+        print("rock beats scissors: You win!)
+            numwinss+=1
+    elif player2_choice 3
