ac2b7fba33f904f1b35002b66db62132bd22ffb9
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-11-18 14:38:41 -0600

    Change to display format

--- player.py
+++ player.py
@@ -11,10 +11,7 @@ class Player:
     def display_and_pick_gestures(self):
         gesture = int(input("\nPlease pick a gesture: \nRock = 1 \nPaper = 2 \nScissors = 3 \nLizard = 4 \nSpock = 5 \n\nChoice: "))
         result = gesture
-        return result
-    
-           
-#   
+        return result  
     
     def choose_game_mode(self):
         i =int(input("\nPlease press 1 for Single Player:  \nPlease press 2 for Multi-Player:\n "))
